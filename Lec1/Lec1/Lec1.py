9d7363178d08153a26071df797e532b085b15385
Vivienne88 <babobigi@naver.com>
2015-09-02 13:48:37 +0900

    Lab1
    Variable types and functions

---
+++ Lec1/Lec1/Lec1.py
@@ -0,0 +1,23 @@
+﻿#salary = float(input("enter salary : "))
+#bonus = float(input("enter bonus : "))
+#paycheck = salary + bonus
+#a = 2.77;
+#print(round(a,0))
+#intput = input("입력하시오 ")
+#print(type(intput))
+#print(paycheck)
+
+#info = "200911317Vivienne"
+#num = info[:9]
+#name = info[9:]
+#print(num+" "+name)
+
+#a = "I eat %-10s apples%%" % "five"
+#print(a)
+
+str=input("종료할까요?")
+str=str.lower()
+print(str)
+
+a=","
+print(a.join("abcd"))
