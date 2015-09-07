3659e32c65567663c48c189fbffaa555a54ea74d
Vivienne88 <babobigi@naver.com>
2015-09-07 13:50:38 +0900

    Lec2
    리스트 수업

---
+++ Lec2/Lec2/Lec2.py
@@ -0,0 +1,46 @@
+﻿#print(data)
+#data[0]='greenjoa'
+#print(data)
+#data[1]=['gj1','gj2']
+#print(data)
+#data=['a','b','c','d',[4,3,2,1]]
+#for i in range(len(data)) :
+#    print(data[i])
+
+#data[4].sort()
+#print(data)
+#data[4].reverse()
+#data.reverse()
+#print(data)
+#print(data)
+#data[1]=['gj1','gj2']
+#data[1:2]=['gj1','gj2']
+
+#data.append('ggggg')
+#print(data)
+
+#data = []
+#for i in range(5) :
+#    data.append(input("데이터를 입력하세요 : "))
+    
+#data.sort();
+#print("TOP 3 : ")
+#for i in range(3) :
+#    print(data[i])
+
+#data = ['a','b',[1,2,3,4,5]]
+#for i in data :
+#    if isinstance(i,list) :
+#        for j in i :
+#            print(j)
+#    else :
+#        print(i)
+
+#data.extend([50])
+#print(data)
+
+t1 = ()
+t2 = (1,)
+print(t1)
+print(t2)
+
