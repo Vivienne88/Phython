7480282bf627316491736e6ab04c1ecd36e02719
Vivienne88 <babobigi@naver.com>
2015-09-16 13:57:07 +0900

    Lec5
    Pickle, File I/O

---
+++ Lec5/Lec5/Lec5.py
@@ -0,0 +1,18 @@
+﻿filename = "GURADA.txt"
+filename2 = "GURADA2.txt"
+import pickle
+
+roles = []
+rolenum = 0
+with open(filename,"r") as fp, open(filename2,"wb+") as pk :
+    for i in fp :
+        (role, message) = i.strip().split(":",1)
+        roles.append(role)
+    pickle.dump(roles,pk)
+
+with open(filename2,"rb") as fp :
+    result = pickle.load(fp)
+    print(result)
+
+           
+
