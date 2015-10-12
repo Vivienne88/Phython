101548f71effe6d1835ede990d4ccf3d9f8317f2
Vivienne88 <babobigi@naver.com>
2015-10-12 19:12:25 +0900

    Lab10
    UNIX 명령어

--- Lab10/Lab10/Lab10.py
+++ Lab10/Lab10/Lab10.py
@@ -2,7 +2,7 @@
 import sys
 import pickle
 import shutil
-
+'''
 os.system("python test.py a b c")
 #print(sys.path.append("경로"))
 
@@ -25,7 +25,7 @@ Y = pickle.load(f)
 print(Y)
 Y.show()
 f.close()
-
+'''
 #print(os.environ)
 #print(list(os.walk(".")))
 
@@ -45,4 +45,85 @@ for i in fl :
 
 #dirname('c:/python34/python.exe')
 
-os.path.dirname('c:/python34/python.exe')
+#os.path.dirname('c:/python34/python.exe')
+import glob
+'''
+os.chdir("C:/Python34")
+print(glob.glob("*.exe"))
+os.chdir("..")
+print(os.getcwd())
+print(glob.glob("*.py"))
+'''
+'''
+import os.path
+ndir = nfile = 0
+def traverse(dir,depth) :
+    global ndir, nfile
+    for obj in glob.glob(dir + '/*'):
+        if depth == 0 :
+            prefix = "|--"
+        else :
+            prefix = "|" + " " * depth + "|--"
+        if os.path.isdir(obj) :
+            ndir += 1
+            print(prefix + os.path.basename(obj))
+            traverse(obj,depth+1)
+        elif os.path.isfile(obj) :
+            nfile += 1
+            print(prefix + os.path.basename(obj))
+        else :
+            print(prefix + 'unknown object:',obj)
+
+    if __name__ == '__main__' :
+        traverse('..',0)
+        print('\n',ndir,'directories', nfile, 'files' )
+
+traverse("C:/Python34",0)
+'''
+'''
+import tempfile
+with tempfile.TemporaryFile('w+',delete=False) as fp:
+    fp.write("Hello World")
+    fp.seek(0)
+    data3 = fp.name
+    print(data3)
+    data2 = fp.read()
+'''  
+'''
+import time
+time1 = time.ctime(999999999)
+t = time.strptime(time1)
+t3 =time.strftime("%Y %m %d %I %M",time.localtime())
+print(t3)
+print(time1)
+print(t)
+
+'''
+'''
+import calendar
+#print(calendar.calendar(2000))
+calendar.prcal(2000)
+calendar.prmonth(2015,10)
+'''
+'''
+import random
+R = random.sample(range(101),10)
+print(R)
+random.shuffle(R)
+print(R)
+print(random.choice(R))
+'''
+import random
+data = [('RED',3),('BLUE',1),('GREEN',4),('YELLOW',2)]
+datalist = []
+'''
+for val, cnt in data:
+    for i in range(cnt):
+        datalist.append(val)
+'''
+datalist = [val for val, cnt in data for i in range(cnt)]
+
+
+print(datalist)
+random.shuffle(datalist)
+print(random.choice(datalist))
