6695136cd630e5e4a55f121212c30b1de98c8d64
Vivienne88 <babobigi@naver.com>
2015-11-16 13:50:33 +0900

    Lab17
    Web Server, Flask, Virtual Environnment

---
+++ Lab17/Lab17/Lab17.py
@@ -0,0 +1,24 @@
+﻿from flask import Flask, url_for, redirect
+app = Flask(__name__)
+
+#웹 루트 접속했을 때
+@app.route('/')
+#Hello World함수 호출
+def hello_world():
+   return redirect(url_for('login'))
+
+@app.route('/login/')
+def login():
+    return 'login'
+
+@app.route('/profile/<username>')
+def get_profile(username):
+    return 'profile : ' + username
+
+@app.route('/message/<int:message_id>')
+def get_message(message_id):
+    return 'message id : %d' % message_id
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host = '203.252.166.42')
