fb17d0c761da116b8420c2d6163ce5649ddb3b5c
Vivienne88 <babobigi@naver.com>
2015-11-18 13:45:15 +0900

    Lab18
    Flask안에 제어문 사용
    data사용
    상속

---
+++ lab18/lab18/lab18.py
@@ -0,0 +1,17 @@
+﻿from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/<name>')
+@app.route('/')
+def hello(name = None) :
+    data = [dict(href="http://www.naver.com",caption="네이버"), dict(href="http://www.google.com",caption="구글")]
+    data2 = {
+        'name' : 'SHY', 
+        'skill' : 'Python'
+    }
+    #return render_template('hello.html',items = data, **data2)
+    return render_template('main.html',items = data)
+if __name__ == '__main__':
+    #app.debug = True
+    app.run();
